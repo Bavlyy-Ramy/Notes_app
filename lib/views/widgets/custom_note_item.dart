4865c54c5d4d6ae458b9753a2f737440de360b08
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-01-26 22:00:39 +0200

    create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 10),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 14, bottom: 10),
+              child: Text(
+                'Build your Career with Bavly Ramy',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.6), fontSize: 19),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 26,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May21 ,2025',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 16,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
