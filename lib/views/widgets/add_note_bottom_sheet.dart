3560d3b76e6443af171cf95cb69a81308cbd4aee
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-01-26 23:29:53 +0200

    create edit note view

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -38,4 +38,3 @@ class AddNoteBottomSheet extends StatelessWidget {
     );
   }
 }
-
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,53 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 10),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 14, bottom: 10),
-              child: Text(
-                'Build your Career with Bavly Ramy',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(0.6), fontSize: 19),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 26,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNotesView();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21 ,2025',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.6),
-                fontSize: 16,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 10),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 14, bottom: 10),
+                child: Text(
+                  'Build your Career with Bavly Ramy',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.6), fontSize: 19),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 26,
+                ),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21 ,2025',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.6),
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
