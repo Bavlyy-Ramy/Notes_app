9477115d71e0c8f43b62ec3b17bb181f391b4ee0
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-01-29 19:14:50 +0200

    created fetch notes cubit

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -1,4 +1,4 @@
-part of 'notes_cubit_cubit.dart';
+part of 'notes_cubit.dart';
 
 @immutable
 sealed class NotesState {}
@@ -6,16 +6,13 @@ sealed class NotesState {}
 final class NotesInitial extends NotesState {}
 final class NotesLoading extends NotesState {}
 final class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
+ final List<NoteModel> notes;
 
   NotesSuccess(this.notes);
 
 }
 final class NotesFailure extends NotesState {
   final String errMessage;
+
   NotesFailure(this.errMessage);
-
 }
-
-
-
