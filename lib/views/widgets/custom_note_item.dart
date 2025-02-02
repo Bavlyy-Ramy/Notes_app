b1d18e507d3a1d705deaf83c14272f89e25778e6
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-02-03 00:18:45 +0200

    formate date and time

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,10 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({
+    Key? key,
+    required this.note,
+  }) : super(key: key);
 
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,21 +28,21 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 10),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
+              title: Text(
+                note.title,
                 style: TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 14, bottom: 10),
                 child: Text(
-                  'Build your Career with Bavly Ramy',
+                  note.subTitle,
                   style: TextStyle(
                       color: Colors.black.withOpacity(0.6), fontSize: 19),
                 ),
@@ -52,7 +59,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May21 ,2025',
+                note.date,
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.6),
                   fontSize: 16,
